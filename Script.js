da17a39761666fb202945e10922c4f3148432abf
mamoon18 <Mamon@users.noreply.github.com>
2020-07-26 15:13:25 +0400

    first commit

---
+++ Script.js
@@ -0,0 +1,110 @@
+//let firstname = 'mamons';
+//let lastname = 'sleat';
+
+
+//console.log(firstname + " " +
+
+//lastname);
+
+
+
+
+
+//let person = {
+//name: 'ahmed',
+//age: 23
+
+//}
+//person.name = 'ali'
+//let getpersoName = person['name']
+//console.log(getpersoName);
+
+
+//let x = 20;
+//let y = 20;
+
+//console.log(x - y);
+//console.log(x + y);
+//console.log(x * y);
+//console.log(x / y);
+//console.log(x ** y);
+//console.log(++x);
+//console.log(x++);
+//x += 5;
+//x -= 5;
+//x *= 5;
+//console.log(x);
+//console.log(x > 0);
+//console.log(x < 0);
+//console.log(x === 0);
+//console.log(x !== 0);
+//console.log(1 === 1);
+//console.log('1' === 1);
+//console.log('----');
+//console.log(1 == 1);
+//console.log('1' == 1);
+//let money = 200
+//let phone = money >= 2000 ? 'iphone' : 'samsung';
+//let phone = money <= 2000 ? 'iphone' : 'samsung';
+//console.log(phone);
+//console.log(true && true);
+//console.log(true && false);
+//console.log(false && false);
+
+
+//console.log(true || false);
+//console.log(true || true);
+//console.log(false || false);
+
+//let x = true;
+//let y = false;
+
+//let a = !y;
+//console.log(a);
+
+//let name = '';
+
+//if (name === 'Ahmad') {
+//console.log('hello Ahmad');
+//} //else if (name === 'Ali') {
+//console.log('hello Ali');
+//} //else if (name === 'Mamon') {
+//console.log('hi Mamon');
+
+//} //else {
+//console.log('no name');
+//}
+let name = 'mamon';
+switch (name) {
+    case 'ahmed':
+        console.log('hello ahmed');
+        break;
+    case 'mamon':
+        console.log('hello mamon');
+        break;
+    case 'ali':
+        console.log('hello Ali');
+        break;
+    default:
+        console.log('no name');
+        break;
+}
+//for (let i = 0; i < 10; i++) {
+//console.log('hello ' + i);
+//}
+//console.log("------------");
+//for (let i = 10; i > 0; i--) {
+//console.log('hello ' + i);
+//}
+//console.log("**********")
+//let i = 0;
+//while (i <= 5) {
+//console.log("hello" + i);
+//i++;
+//console.log("**********")
+//}
+let i = 5;
+while (i >= 0) {
+    console.log("hello" + i);
+    i--;
+}
